39ef63d198c76ccff3de7cf53f78a7d48cc592df
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-13 11:30:16 +0530

    Create KmToMiles.java

---
+++ KmToMiles.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class KmToMiles {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter distance in kilometers:");
+        double kilometers = input.nextDouble();
+
+        double miles = kilometers * 0.621371;
+
+        System.out.println("Distance in miles: " + miles);
+
+        input.close();
+    }
+}
