57dd7ed82813884a0ada7e5f40559eca3f2fda9b
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-13 11:42:43 +0530

    Create SamsAverageMark.java

---
+++ ProgrammingElements/SamsAverageMark.java
@@ -0,0 +1,13 @@
+public class SamsAverageMark {
+
+    public static void main(String[] args) {
+        double mathsMark = 94;
+        double physicsMark = 95;
+        double chemistryMark = 96;
+
+        double totalMarks = mathsMark + physicsMark + chemistryMark;
+        double averageMark = totalMarks / 3;
+
+        System.out.println("Sam’s average mark in PCM is " + averageMark);
+    }
+}
