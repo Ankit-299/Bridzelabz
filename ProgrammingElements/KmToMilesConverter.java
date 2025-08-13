d2952fc13ef939f2e934af60e4658d5ca5df134d
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-13 11:43:45 +0530

    Create KmToMilesConverter.java

---
+++ ProgrammingElements/KmToMilesConverter.java
@@ -0,0 +1,9 @@
+public class KmToMilesConverter {
+
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 0.621371;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
