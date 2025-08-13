edb130facb8631113cd97b9779827af35fbce18a
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-13 12:06:56 +0530

    Create TotalPurchasePriceCalculator.java

---
+++ ProgrammingElements/TotalPurchasePriceCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TotalPurchasePriceCalculator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double unitPrice;
+        int quantity;
+
+        System.out.println("Enter the unit price of the item (in INR):");
+        unitPrice = input.nextDouble();
+
+        System.out.println("Enter the quantity to be bought:");
+        quantity = input.nextInt();
+
+        double totalPrice = unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR " + totalPrice + " if the quantity " + quantity + " and unit price is INR " + unitPrice);
+
+        input.close();
+    }
+}
