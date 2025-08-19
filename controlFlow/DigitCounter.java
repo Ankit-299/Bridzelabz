efc2a114d7d0b443728c1b9ae048036165066fad
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-19 12:32:53 +0530

    Create DigitCounter.java

---
+++ controlFlow/DigitCounter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DigitCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        int count = 0;
+        int tempNumber = number;
+
+        if (tempNumber == 0) {
+            count = 1;
+        } else {
+            if (tempNumber < 0) {
+                tempNumber = -tempNumber;
+            }
+            while (tempNumber != 0) {
+                tempNumber /= 10;
+                count++;
+            }
+        }
+        System.out.println("The number of digits is: " + count);
+        scanner.close();
+    }
+}
