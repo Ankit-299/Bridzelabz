d445e6af457e6c6420eef58b5cd5d5115569faa1
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-19 12:40:05 +0530

    Create AbundantNumberChecker.java

---
+++ controlFlow/AbundantNumberChecker.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AbundantNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        int sumOfDivisors = 0;
+
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    sumOfDivisors += i;
+                }
+            }
+
+            if (sumOfDivisors > number) {
+                System.out.println(number + " is an Abundant Number.");
+            } else {
+                System.out.println(number + " is not an Abundant Number.");
+            }
+        }
+        scanner.close();
+    }
+}
