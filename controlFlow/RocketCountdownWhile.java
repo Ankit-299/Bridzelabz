677c2868ff761bcfd9d5e11fb219ca26820e8c6b
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-19 11:51:54 +0530

    Create RocketCountdownWhile.java

---
+++ controlFlow/RocketCountdownWhile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RocketCountdownWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter countdown start value: ");
+        int counter = scanner.nextInt();
+
+        while (counter >= 1) {
+            System.out.println(counter + "...");
+            counter--;
+        }
+        System.out.println("Liftoff!");
+        scanner.close();
+    }
+}
