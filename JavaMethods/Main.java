8a4968f4ab01e6deac455b6f55ef146f7c227f5b
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-25 11:32:11 +0530

    Create  Main.java

---
+++ JavaMethods/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Football Heights ===");
+        FootballHeights.run();
+
+        System.out.println("\n=== Number Checker ===");
+        NumberChecker.run();
+
+        System.out.println("\n=== OTP Generator ===");
+        OTPGenerator.run();
+
+        System.out.println("\n=== Calendar Display ===");
+        CalendarDisplay.run(7, 2005);
+
+        System.out.println("\n=== Collinear Points ===");
+        CollinearPoints.run();
+
+        System.out.println("\n=== Bonus Calculator ===");
+        BonusCalculator.run();
+
+        System.out.println("\n=== Distance & Line Equation ===");
+        DistanceLineEquation.run();
+
+        System.out.println("\n=== Student Marks ===");
+        StudentMarks.run();
+
+        System.out.println("\n=== Matrix Basic Ops ===");
+        MatrixBasicOps.run();
+
+        System.out.println("\n=== Matrix Advanced Ops ===");
+        MatrixAdvancedOps.run();
+    }
+}
