600a4f829b40b324d7776cbbb28516b7c047697a
Ankit-299 <bhardwajankit5517@gmail.com>
2025-08-25 11:16:08 +0530

    Create BMICalculator.java

---
+++ JavaMethods/BMICalculator.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class BMICalculator {
+
+    public static double calcBMI(double weight, double heightCm) {
+        double heightM = heightCm / 100.0;
+        return weight / (heightM * heightM);
+    }
+
+    public static String getStatus(double bmi) {
+        if (bmi < 18.5) return "Underweight";
+        else if (bmi < 24.9) return "Normal";
+        else if (bmi < 29.9) return "Overweight";
+        else return "Obese";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[][] data = new double[10][3]; // col0=weight, col1=height, col2=BMI
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter weight(kg) for person " + (i + 1) + ": ");
+            data[i][0] = sc.nextDouble();
+            System.out.print("Enter height(cm) for person " + (i + 1) + ": ");
+            data[i][1] = sc.nextDouble();
+
+            data[i][2] = calcBMI(data[i][0], data[i][1]);
+        }
+
+        System.out.println("\n--- BMI Report ---");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Person " + (i + 1) +
+                               " | Weight: " + data[i][0] +
+                               "kg | Height: " + data[i][1] + "cm" +
+                               " | BMI: " + String.format("%.2f", data[i][2]) +
+                               " | Status: " + getStatus(data[i][2]));
+        }
+    }
+}
