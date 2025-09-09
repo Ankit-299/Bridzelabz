19a05f53499976ed836d4268412e6d90ea01d79b
Ankit-299 <bhardwajankit5517@gmail.com>
2025-09-09 11:21:32 +0530

    Create Circle.java

---
+++ java constructors and inheritance/Circle.java
@@ -0,0 +1,25 @@
+class Circle {
+    double radius;
+
+    Circle() {
+        this(1.0);
+    }
+
+    Circle(double r) {
+        this.radius = r;
+    }
+
+    void display() {
+        System.out.println("Radius: " + radius);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        c1.display();
+
+        Circle c2 = new Circle(5.5);
+        c2.display();
+    }
+}
