62c9ca4080e79be938ecaef106311303cfcf459d
Ankit-299 <bhardwajankit5517@gmail.com>
2025-09-21 22:07:39 +0530

    Create  AttendanceTracker.java

---
+++ Scenario-basedProblems/AttendanceTracker.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class AttendanceTracker {
+    public static Map<String, Integer> calculateAttendance(List<String>... days) {
+        Map<String, Integer> attendance = new HashMap<>();
+        for (List<String> day : days) {
+            for (String student : day) {
+                attendance.put(student, attendance.getOrDefault(student, 0) + 1);
+            }
+        }
+        return attendance;
+    }
+
+    public static void main(String[] args) {
+        List<String> day1 = Arrays.asList("Aman", "Neha", "Ravi");
+        List<String> day2 = Arrays.asList("Neha", "Ravi", "Simran");
+        List<String> day3 = Arrays.asList("Aman", "Ravi");
+        Map<String, Integer> result = calculateAttendance(day1, day2, day3);
+
+        for (Map.Entry<String, Integer> entry : result.entrySet()) {
+            System.out.println(entry.getKey() + " attended " + entry.getValue() + " days");
+        }
+    }
+}
